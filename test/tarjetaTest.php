cc0cc0bfd8f263487e3d97602d25e405ff674501
NicoFiderio <nicofide97@gmail.com>
2016-10-19 14:35:50 -0300

    Agregando pagar sin saldo y modificando pagar a pagar con transbordo

--- test/tarjetaTest.php
+++ test/tarjetaTest.php
@@ -3,7 +3,7 @@ namespace Poli\Tarjeta;
 
 class TarjetaTest extends \PHPUnit_Framework_TestCase {
 
-  protected $tarjeta,$colectivo1;	
+  protected $tarjeta,$colectivo1,$colectivo2;	
 
   public function setup(){
 			$this->tarjeta = new Baja();
@@ -12,14 +12,14 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
   }	
 
   public function testRecargar() {
-    $this->tarjeta->recargar(272);
-    $this->assertEquals($this->tarjeta->saldo(), 320, "Cuando cargo 272 deberia tener finalmente 320");
-    $this->tarjeta = new Baja();
-    $this->tarjeta->recargar(273);
-    $this->assertEquals($this->tarjeta->saldo(), 273, "Cuando cargo 273 deberia tener finalmente 273");
+	$this->tarjeta->recargar(272);
+	$this->assertEquals($this->tarjeta->saldo(), 320, "Cuando cargo 272 deberia tener finalmente 320");
+	$this->tarjeta = new Baja();
+	$this->tarjeta->recargar(273);
+	$this->assertEquals($this->tarjeta->saldo(), 273, "Cuando cargo 273 deberia tener finalmente 273");
   }
 
- public function testPagar() {
+ public function testPagarConTransbordo() {
   	$this->tarjeta= new Baja();
 	$this->tarjeta->recargar(272);
   	$this->tarjeta->pagar($this->colectivo1, "2016/09/29 11:05");
@@ -27,6 +27,14 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
 	$this->tarjeta->pagar($this->colectivo2, "2016/09/29 11:15");
 	$this->assertEquals($this->tarjeta->saldo(), 309.5, "Cuando pago un colectivo con transbordo deberia tener finalmente 309.5");  	
   }
+ public function testPagarSinSaldoEnLaTarjeta(){
+	$this->tarjeta->pagar($this->colectivo1,"2016/09/29 06:00");
+	$this->tarjeta->pagar($this->colectivo2,"2016/09/29 08:00");
+	$this->tarjeta->pagar($this->colectivo1,"2016/09/29 10:00");
+	$this->assertEquals($this->tarjeta->viajePLus(), 2, "Si marco en 3 colectivos sin tener saldo deberia poder pagar nada mas los dos primeros");
+	$this->tarjeta->recargar(20);
+	$this->assertEquals($this->tarjeta->saldo(), 4, "Si cargo 20 y habia usado dos viajes plus deberia tener cobrarme los dos plus y tener finalmente 4");
+ }
 }
 
 ?>
